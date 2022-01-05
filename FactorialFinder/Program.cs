bc646a496c2fd8d57ef6bf9424f50730517915a0
fulfordcs <fulfordcs@gmail.com>
2022-01-05 09:57:15 +0000

    Added instructions

--- FactorialFinder/Program.cs
+++ FactorialFinder/Program.cs
@@ -10,6 +10,11 @@ namespace FactorialFinder
     {
         static void Main(string[] args)
         {
+            /*
+             * The Factorial of a positive integer, n, is defined as the product of the sequence n, n-1, n-2, ...1 and the factorial of zero, 0, is defined as being 1. 
+             * Source: Challenge 1 from https://live.withcode.uk/alevel/
+             */
+
         }
     }
 }
